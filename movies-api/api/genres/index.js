fb6f46e00e28e2a175984b3d0dbcc95050e493f7
kasdal <plesmilan@hotmail.com>
2021-12-02 18:47:24 +0000

    Finished exercise for genres

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { genres } from './genresData';
+import Genre from './genreModel';
 import uniqid from 'uniqid'
 
 const router = express.Router(); 
---
+++ movies-api/seedData/genres.js
@@ -0,0 +1,19 @@
+const genres= [
+    {
+        "id": 22,
+        "name": "RPG"
+    },
+    {
+        "id": 33,
+        "name": "Comedy"
+    },
+    {
+        "id": 45,
+        "name": "Animation"
+    },
+    {
+        "id": 11,
+        "name": "Action"
+    }
+]
+export default genres;
